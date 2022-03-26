417f77262cdd052013a3ebd1ac5857e281675b5c
LekoArts <lekoarts@gmail.com>
2022-03-26 11:15:51 +0100

    Initial commit from gatsby

---
+++ gatsby-config.ts
@@ -0,0 +1,11 @@
+import type { GatsbyConfig } from "gatsby";
+
+const config: GatsbyConfig = {
+  siteMetadata: {
+    title: ``,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: ["gatsby-plugin-vanilla-extract"]
+};
+
+export default config;
