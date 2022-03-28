dfeb2809d9093f62d94ce24f23e16227c6ca0784
LekoArts <lekoarts@gmail.com>
2022-03-28 17:11:19 +0200

    Add tutorial code

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -2,10 +2,10 @@ import type { GatsbyConfig } from "gatsby";
 
 const config: GatsbyConfig = {
   siteMetadata: {
-    title: ``,
+    title: `Getting Started with TypeScript in Gatsby`,
     siteUrl: `https://www.yourdomain.tld`
   },
-  plugins: ["gatsby-plugin-vanilla-extract"]
+  plugins: [`gatsby-plugin-vanilla-extract`]
 };
 
 export default config;
